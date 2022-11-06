5a30ea934c45897a45b81e482b8ef3c761857455
sm2k4 <shrinibasmahanta2004@gmail.com>
2022-11-07 00:50:55 +0530

    fetching data from firebase.

--- todoer/lib/screens/home.dart
+++ todoer/lib/screens/home.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:todoer/screens/addtask.dart';
 
 class Home extends StatefulWidget {
@@ -10,7 +12,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String uid = '';
+
   @override
+  void initState() {
+    getuid();
+    super.initState();
+  }
+
+  getuid() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+    setState(() {
+      uid = user!.uid;
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -18,9 +35,57 @@ class _HomeState extends State<Home> {
           elevation: 0,
         ),
         body: Container(
+          // ignore: sort_child_properties_last
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('tasks')
+                .doc(uid)
+                .collection('mytasks')
+                .snapshots(),
+            builder: ((context, snapshots) {
+              if (snapshots.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator.adaptive(),
+                );
+              } else {
+                final documents = snapshots.data!.docs;
+
+                return ListView.builder(
+                  itemCount: documents.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.all(15),
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.blue,
+                      ),
+                      margin: EdgeInsets.all(10),
+                      child: Row(
+                        children: [
+                          Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                    margin: EdgeInsets.only(left: 25),
+                                    child: Text(
+                                      documents[index]['title'],
+                                      style:
+                                          GoogleFonts.comfortaa(fontSize: 16),
+                                    ))
+                              ]),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              }
+            }),
+          ),
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          color: Colors.blueAccent,
+          color: Colors.white,
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.miniEndTop,
         floatingActionButton: SizedBox(
