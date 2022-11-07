d16ad242bdbf1e87f25a12ce7ba45106bc43f248
sm2k4 <shrinibasmahanta2004@gmail.com>
2022-11-08 02:19:26 +0530

    slidable delete button added.

--- todoer/lib/screens/home.dart
+++ todoer/lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:todoer/screens/addtask.dart';
@@ -68,79 +69,87 @@ class _HomeState extends State<Home> {
                                     )));
                       },
                       child: Container(
-                        padding: EdgeInsets.all(15),
-                        height: 100,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.blue,
-                        ),
                         margin: EdgeInsets.all(10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Container(
-                                  child: Checkbox(
-                                    value: documents[index].get('checkbox'),
-                                    onChanged: (p0) async {
-                                      await FirebaseFirestore.instance
-                                          .collection('tasks')
-                                          .doc(uid)
-                                          .collection('mytasks')
-                                          .doc(documents[index]['time'])
-                                          .update({
-                                        'checkbox':
-                                            !documents[index].get('checkbox')
-                                      });
-                                    },
-                                  ),
-                                ),
-                              ],
+                        child: Slidable(
+                          endActionPane:
+                              ActionPane(motion: StretchMotion(), children: [
+                            SlidableAction(
+                              onPressed: (p0) async {
+                                await FirebaseFirestore.instance
+                                    .collection('tasks')
+                                    .doc(uid)
+                                    .collection('mytasks')
+                                    .doc(documents[index]['time'])
+                                    .delete();
+                              },
+                              icon: Icons.delete,
+                              backgroundColor: Colors.red,
+                              padding: EdgeInsets.all(15),
+                              borderRadius: BorderRadius.circular(15),
+                            )
+                          ]),
+                          child: Container(
+                            padding: EdgeInsets.all(15),
+                            //height: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Colors.blue,
                             ),
-                            Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                      margin: EdgeInsets.only(left: 25),
-                                      child: Text(
-                                        documents[index]['title'],
-                                        style: TextStyle(
-                                            fontFamily: 'comfortaa',
-                                            fontSize: 20,
-                                            decoration:
-                                                documents[index].get('checkbox')
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      child: Checkbox(
+                                        value: documents[index].get('checkbox'),
+                                        onChanged: (p0) async {
+                                          await FirebaseFirestore.instance
+                                              .collection('tasks')
+                                              .doc(uid)
+                                              .collection('mytasks')
+                                              .doc(documents[index]['time'])
+                                              .update({
+                                            'checkbox': !documents[index]
+                                                .get('checkbox')
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                          margin: EdgeInsets.only(left: 25),
+                                          child: Text(
+                                            documents[index]['title'],
+                                            style: TextStyle(
+                                                fontFamily: 'comfortaa',
+                                                fontSize: 20,
+                                                decoration: documents[index]
+                                                        .get('checkbox')
                                                     ? TextDecoration.lineThrough
                                                     : TextDecoration.none),
 
-                                        //GoogleFonts.comfortaa(fontSize: 17),
-                                      )),
-                                  SizedBox(
-                                    height: 5,
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.only(left: 25),
-                                    child: Text(
-                                        DateFormat.yMd().add_jm().format(time)),
-                                  ),
-                                ]),
-                            Container(
-                              child: IconButton(
-                                  onPressed: () async {
-                                    await FirebaseFirestore.instance
-                                        .collection('tasks')
-                                        .doc(uid)
-                                        .collection('mytasks')
-                                        .doc(documents[index]['time'])
-                                        .delete();
-                                  },
-                                  icon: Icon(
-                                    Icons.delete_outline_rounded,
-                                    color: Colors.redAccent,
-                                  )),
-                            )
-                          ],
+                                            //GoogleFonts.comfortaa(fontSize: 17),
+                                          )),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.only(left: 25),
+                                        child: Text(DateFormat.yMd()
+                                            .add_jm()
+                                            .format(time)),
+                                      ),
+                                    ]),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
                     );
